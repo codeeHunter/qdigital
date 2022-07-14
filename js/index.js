a52c86ef1482d07400c778f241486bb403347763
codeeHunter <chishok32@gmail.com>
2022-07-14 17:51:29 +0400

    Финальная версия

--- js/index.js
+++ js/index.js
@@ -22,8 +22,9 @@ function SubTask(id, description, time) {
   this.time = time;
 }
 
-function Task(id, name) {
+function Task(id, visible, name) {
   this.id = id;
+  this.visible = visible;
   this.name = name;
   this.subTasks = [];
 }
@@ -75,16 +76,16 @@ const onChange = (id, name) => {
 const createList = (task) => {
   return `
     <div class="taskItem id=${task.id}">
-    <div class="taskLogo"></div>
-    <div class="taskNameList" >
+      <div class="taskLogo"></div>
+      <div class="taskNameList" >
         <h4>${task.name}</h4>
         <ul>
-            ${renderItems(task.subTasks)}
+          ${renderItems(task.subTasks)}
         </ul>
-    </div>
-        <div class="load">
-            <button onClick="loadLocal()" class="clearButton loadButton">Load</button>
-        </div>
+      </div>
+      <div class="load">
+        <button onClick="loadLocal()" class="clearButton loadButton">Load</button>
+      </div>
     </div>
     `;
 };
@@ -117,53 +118,38 @@ const fillTask = () => {
 };
 
 let iterator = 0;
-let k = 0;
-const loadLocal = () => {
-  let newArr = [];
-  let ic = 0;
-  todoList = JSON.parse(localStorage.getItem("tasks"));
-  if (k != 2) {
-    k = 2;
-    for (const obj of todoList) {
-      for (let i = 0; i < tasks.length; i++) {
-        if (obj.id !== tasks[i].id) {
-          const task = new Task(obj.id, obj.name);
-          task.subTasks = obj.subTasks;
-          newArr.push(task);
-        }
-      }
-    }
-    if (iterator == 1) {
-      for (let i = newArr.length - iterator; i >= 0; i--) {
-        tasks.push(newArr[i]);
-      }
-    } else if (iterator > 1) {
-      for (let i = newArr.length - iterator - 1; i >= 0; i--) {
-        tasks.push(newArr[i]);
-      }
-    } else {
-      alert("Данных в localStorage нет");
-    }
 
-    subTasks = [];
-    subTasks.push(new SubTask(magicIndex, "", ""));
-    addSubTask();
-    fillTask();
-  } else {
-    alert("Все данные выведены.");
+const loadLocal = () => {
+  let todoList = JSON.parse(localStorage.getItem("tasks"));
+  let newTodo = [];
+
+  for (let i = 0; i < todoList.length; i++) {
+    if (todoList[i].visible == false) {
+      todoList[i].visible = true;
+      newTodo.push(todoList[i]);
+    }
   }
-  console.log(tasks);
+  localStorage.setItem("tasks", JSON.stringify(todoList));
+  newTodo.map((i) => tasks.push(i));
+
+  subTasks = [];
+  subTasks.push(new SubTask(magicIndex, "", ""));
+  addSubTask();
+  fillTask();
+  iterator = 1;
 };
 
-let p = 0;
-let t = 0;
+function uniqBy(a, key) {
+  var seen = {};
+  return a.filter(function (item) {
+    var k = key(item);
+    return seen.hasOwnProperty(k) ? false : (seen[k] = true);
+  });
+}
 
 saveLocal.addEventListener("click", () => {
-  let todo = JSON.parse(localStorage.getItem("tasks"));
-  let todoList = todo;
-  console.log(todo);
   if (taskName.value !== "") {
-    const task = new Task(setId(), taskName.value);
+    const task = new Task(setId(), true, taskName.value);
     task.subTasks = subTasks;
     tasks.push(task);
     tasks.reverse();
@@ -175,21 +161,19 @@ saveLocal.addEventListener("click", () => {
   } else {
     alert("Введите имя задачи!");
   }
-  iterator++;
+
+  let todo = JSON.parse(localStorage.getItem("tasks"));
+  let newTasksList = [];
   if (todo === null) {
     localStorage.setItem("tasks", JSON.stringify(tasks));
   } else {
-    for (let i = todoList.length - 1; i <= todoList.length; i++) {
-      for (let j = 0; j < tasks.length; j++) {
-        if (tasks[j].id !== todoList[i].id) {
-          todoList.push(tasks[j]);
-        }
+    for (let i = 0; i < tasks.length; i++) {
+      if (todo.indexOf(tasks[i]) === -1) {
+        todo.push(tasks[i]);
       }
-      break;
     }
-    if (todoList === todo) {
-      localStorage.setItem("tasks", JSON.stringify(todoList));
-    }
+    newTasksList = uniqBy(todo, JSON.stringify);
+    localStorage.setItem("tasks", JSON.stringify(newTasksList));
   }
 });
 
@@ -199,10 +183,8 @@ subTask.addEventListener("click", () => {
 });
 
 addTask.addEventListener("click", () => {
-  k++;
-  iterator++;
   if (taskName.value !== "") {
-    const task = new Task(setId(), taskName.value);
+    const task = new Task(setId(), true, taskName.value);
     task.subTasks = subTasks;
     tasks.push(task);
     tasks.reverse();
@@ -217,6 +199,13 @@ addTask.addEventListener("click", () => {
 });
 
 window.onload = () => {
+  let todo = JSON.parse(localStorage.getItem("tasks"));
+  if (todo !== null) {
+    for (let i = 0; i < todo.length; i++) {
+      todo[i].visible = false;
+    }
+    localStorage.setItem("tasks", JSON.stringify(todo));
+  }
   subTasks.push(new SubTask(magicIndex, "", ""));
   addSubTask();
 };
